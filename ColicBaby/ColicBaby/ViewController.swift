b36d2b7574629602862b048cb1537785392e9f9b
Mehmet Yazık <mhmtyzk@gmail.com>
2019-10-21 18:02:49 +0300

    Sadece proje oluşturuldu

---
+++ ColicBaby/ColicBaby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColicBaby
+//
+//  Created by Mehmet Yazık on 21.10.2019.
+//  Copyright © 2019 Mehmet Yazık. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
